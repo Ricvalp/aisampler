530019c5e36a1f85b82ad05f696abf886193e03c
ricvalp <valperga.riccardo@gmail.com>
2024-07-05 23:05:29 +0200

    final commits

--- aisampler/sampling/__init__.py
+++ aisampler/sampling/__init__.py
@@ -1,9 +1,9 @@
 from aisampler.sampling.hamiltonian_monte_carlo import hmc
 from aisampler.sampling.metrics import ess, gelman_rubin_r, effective_sample_size
-from aisampler.sampling.metropolis_hastings_with_momentum import (
+from aisampler.sampling.metropolis_hastings_with_momentum_ import (
     metropolis_hastings_with_momentum,
 )
-from aisampler.sampling.metropolis_hastings_with_momentum_fn import get_sample_fn
+from aisampler.sampling.metropolis_hastings_with_momentum import get_sample_fn
 from aisampler.sampling.utils import (
     plot_samples_with_density,
     plot_kde,
--- aisampler/sampling/metropolis_hastings_with_momentum.py
+++ aisampler/sampling/metropolis_hastings_with_momentum.py
@@ -1,136 +1,102 @@
-import time
-
 import jax
 import jax.numpy as jnp
-from absl import logging
 
 
-def mh_kernel_with_momentum(
-    x: jnp.ndarray,
-    key: jnp.ndarray,
-    cov_p: jnp.ndarray,
-    kernel: callable,
-    density: callable,
-    parallel_chains: int = 100,
-) -> tuple:
-    """
-    Metropolis-Hastings kernel with momentum for a given kernel and density.
-
-    Args:
-
-        x: jnp.ndarray
-        key: jnp.ndarray
-        cov_p: jnp.ndarray
-        kernel: callable
-        density: callable
-        parallel_chains: int
-    Returns:
-
-        tuple: Tuple of the new samples, acceptance rate, and the key.
-    """
-
-    key, accept_subkey, momentum_subkey = jax.random.split(key, 3)
-    x_new = kernel(x)
-
-    log_prob_new = density(x_new)
-    log_prob_old = density(x)
-    log_prob_ratio = log_prob_old - log_prob_new  # log_prob_old - log_prob_new
-
-    accept = jax.random.uniform(accept_subkey, (parallel_chains,)) < jnp.exp(
-        log_prob_ratio
-    )
-
-    # accept = (
-    #     jnp.log(jax.random.uniform(accept_subkey, (parallel_chains,))) < log_prob_ratio
-    # )
-
-    x_new = jnp.where(accept[:, None], x_new, x)[:, : x.shape[1] // 2]
-    momentum = jax.random.multivariate_normal(
-        momentum_subkey, jnp.zeros(x.shape[1] // 2), cov_p, (parallel_chains,)
-    )
-    x_new = jnp.concatenate([x_new, momentum], axis=1)
-
-    return x_new, accept.mean(), key
-
-
-jit_mh_kernel_with_momentum = jax.jit(
-    mh_kernel_with_momentum,
-    static_argnums=(
-        3,
-        4,
-        5,
-    ),
-)
-
-
-def metropolis_hastings_with_momentum(
-    kernel: callable,
+def get_sample_fn(
+    kernel_fn: callable,
     density: callable,
     d: int,
     n: int,
     cov_p: jnp.ndarray,
     parallel_chains: int = 100,
     burn_in: int = 100,
-    rng: jnp.ndarray = jax.random.PRNGKey(42),
     initial_std: float = 1.0,
     starting_points: jnp.ndarray = None,
     vstack: bool = True,
-) -> tuple:
+) -> callable:
     """
-
-    Metropolis-Hastings with momentum for a given kernel and density.
+    Function to get the (jitted) Metropolis-Hastings with momentum sampler for a given kernel function and density.
+    The kernel function takes in the current state and the parameters dictionary and returns the new stata.
 
     Args:
-        kernel: callable
-        density: callable
-        d: int
-        n: int
-        cov_p: jnp.ndarray
-        parallel_chains: int
-        burn_in: int
-        rng: jnp.ndarray
-        initial_std: float
-        starting_points: jnp.ndarray
-        vstack: bool
+        kernel_fn (callable):
+        density (callable):
+        d (int):
+        n (int):
+        cov_p (jnp.ndarray):
+        parallel_chains (int, optional): Defaults to 100.
+        burn_in (int, optional): Defaults to 100.
+        initial_std (float, optional): Defaults to 1.0.
+        starting_points (jnp.ndarray, optional): Defaults to None.
+        vstack (bool, optional): Defaults to True.
+
     Returns:
-        tuple: Tuple of samples and acceptance rate.
+        callable:
     """
-    first_init_subkey, second_init_subkey, sampling_subkey = jax.random.split(rng, 3)
 
-    if starting_points is None:
-        x = jax.random.normal(first_init_subkey, (parallel_chains, d)) * initial_std
-        x = jnp.concatenate(
-            [
-                x,
-                jax.random.multivariate_normal(
-                    second_init_subkey, jnp.zeros(d), cov_p, (parallel_chains,)
-                ),
-            ],
-            axis=1,
+    def mh_kernel_with_momentum(
+        x: jnp.ndarray,
+        params: dict,
+        key: jnp.ndarray,
+    ) -> tuple:
+        """ """
+
+        key, accept_subkey, momentum_subkey = jax.random.split(key, 3)
+        x_new = kernel_fn(x, params)
+
+        log_prob_new = density(x_new)
+        log_prob_old = density(x)
+        log_prob_ratio = log_prob_old - log_prob_new
+
+        accept = jax.random.uniform(accept_subkey, (parallel_chains,)) < jnp.exp(
+            log_prob_ratio
         )
-    else:
-        x = starting_points[:parallel_chains]
 
-    jit_mh_kernel_with_momentum(
-        x, sampling_subkey, cov_p, kernel, density, parallel_chains=parallel_chains
-    )
-
-    samples = []
-    ars = []
-
-    for i in range(n + burn_in):
-        x, ar, sampling_subkey = jit_mh_kernel_with_momentum(
-            x, sampling_subkey, cov_p, kernel, density, parallel_chains=parallel_chains
+        x_new = jnp.where(accept[:, None], x_new, x)[:, : x.shape[1] // 2]
+        momentum = jax.random.multivariate_normal(
+            momentum_subkey, jnp.zeros(x.shape[1] // 2), cov_p, (parallel_chains,)
         )
-        if i >= burn_in:
-            samples.append(x)
-            ars.append(ar)
-    if vstack:
-        return jnp.vstack(samples), jnp.array(ars).mean()
-    else:
-        return jnp.transpose(jnp.array(samples), (1, 0, 2)), jnp.array(ars).mean()
+        x_new = jnp.concatenate([x_new, momentum], axis=1)
 
+        return x_new, accept.mean(), key
 
-@jax.jit
-def jitted_stack(x):
-    return jnp.stack(x)
+    jit_mh_kernel_with_momentum = jax.jit(mh_kernel_with_momentum)
+
+    def metropolis_hastings_with_momentum(
+        params: dict,
+        rng: jnp.ndarray = jax.random.PRNGKey(42),
+    ):
+        first_init_subkey, second_init_subkey, sampling_subkey = jax.random.split(
+            rng, 3
+        )
+
+        if starting_points is None:
+            x = jax.random.normal(first_init_subkey, (parallel_chains, d)) * initial_std
+            x = jnp.concatenate(
+                [
+                    x,
+                    jax.random.multivariate_normal(
+                        second_init_subkey, jnp.zeros(d), cov_p, (parallel_chains,)
+                    ),
+                ],
+                axis=1,
+            )
+        else:
+            x = starting_points[:parallel_chains]
+
+        samples = []
+        ars = []
+
+        for i in range(n + burn_in):
+            x, ar, sampling_subkey = jit_mh_kernel_with_momentum(
+                x, params, sampling_subkey
+            )
+            if i >= burn_in:
+                samples.append(x)
+                ars.append(ar)
+        if vstack:
+            return jnp.vstack(samples), jnp.array(ars).mean()
+        else:
+            return jnp.transpose(jnp.array(samples), (1, 0, 2)), jnp.array(ars).mean()
+
+    return metropolis_hastings_with_momentum
